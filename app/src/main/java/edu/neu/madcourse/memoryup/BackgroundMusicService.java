e086007283d5ed73ccf8e7e7066b608fabbc98e0
tpuhov <puhov.t@northeastern.edu>
2022-04-23 12:50:08 -0400

    Switch music when current song ends.

--- app/src/main/java/edu/neu/madcourse/memoryup/BackgroundMusicService.java
+++ app/src/main/java/edu/neu/madcourse/memoryup/BackgroundMusicService.java
@@ -5,17 +5,14 @@ import androidx.annotation.Nullable;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.IBinder;
-import android.widget.Toast;
-
-import java.io.IOException;
 
 
 public class BackgroundMusicService extends Service {
     MediaPlayer musicPlayer;
     int currentSong = 0;
-    static final int[] songs = new int[] {R.raw.the_cradle_of_your_soul};
+    static final int[] songs = new int[] {R.raw.the_cradle_of_your_soul, R.raw.cinematic_atmosphere,
+            R.raw.price_of_freedom, R.raw.just_relax};
 
     @Override
     public void onCreate() {
@@ -23,33 +20,9 @@ public class BackgroundMusicService extends Service {
 
         // Music from https://pixabay.com/music/search/theme/background%20music/
         musicPlayer = MediaPlayer.create(this, songs[currentSong]);
-        musicPlayer.setVolume(100, 100);
 
-        // loop through songs when current finishes
-        musicPlayer.setOnCompletionListener(mp -> {
-            // go to next song, or reset to first one
-            if (currentSong + 1 < songs.length) {
-                ++currentSong;
-            }
-            else {
-                currentSong = 0;
-            }
-
-            // reset player
-            musicPlayer.reset();
-
-            // load next song
-            ++currentSong;
-            String nextSong = "android:resource://" + this.getPackageName() + "/raw/" + songs[currentSong];
-            try {
-                mp.setDataSource(this, Uri.parse(nextSong));
-                mp.prepare();
-                mp.start();
-            } catch (IOException e) {
-                Toast.makeText(this, "Failed to load song", Toast.LENGTH_SHORT).show();
-                e.printStackTrace();
-            }
-        });
+        // set listener to move to next song
+        musicPlayer.setOnCompletionListener(this::onCompletion);
         musicPlayer.start();
     }
 
@@ -63,5 +36,23 @@ public class BackgroundMusicService extends Service {
     public void onDestroy() {
         super.onDestroy();
         musicPlayer.release();
+    }
+
+    // move to next song when current one ends
+    private void onCompletion(MediaPlayer mp) {
+        // go to next song, or reset to first one
+        if (currentSong + 1 < songs.length) {
+            ++currentSong;
+        } else {
+            currentSong = 0;
+        }
+
+        // release finished song
+        musicPlayer.release();
+
+        // load and start next one
+        musicPlayer = MediaPlayer.create(this, songs[currentSong]);
+        musicPlayer.setOnCompletionListener(this::onCompletion);
+        musicPlayer.start();
     }
 }
